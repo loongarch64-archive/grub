4f54c1ff7a2ec31eea9e3084353a8288f56b06b5
zeiwai <zylinux94@gmail.com>
2021-05-27 16:13:22 +0800

    add LoongArch64 support for grub

--- util/grub-install-common.c
+++ util/grub-install-common.c
@@ -776,6 +776,7 @@ static struct
     [GRUB_INSTALL_PLATFORM_ARM_COREBOOT] =     { "arm",     "coreboot"  },
     [GRUB_INSTALL_PLATFORM_RISCV32_EFI] =      { "riscv32", "efi"       },
     [GRUB_INSTALL_PLATFORM_RISCV64_EFI] =      { "riscv64", "efi"       },
+    [GRUB_INSTALL_PLATFORM_LOONGARCH64_EFI] =  { "loongarch64", "efi"   },
   }; 
 
 char *
